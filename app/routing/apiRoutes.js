c074ce8a3bf70636c6ef7a2c3942f146ec6e8b63
thaining7 <thaining@protonmail.com>
2019-04-22 18:41:54 -0500

    creating heroku

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var friendData = require("../data/friends");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        // Could add  Server-side validation before pushing to array
+        friendData.push(req.body);
+        res.json(req.body);
+    });
+
+};
