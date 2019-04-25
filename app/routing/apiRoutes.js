ddfb445390c92e322d03f967b374f66611c50f5b
thaining7 <thaining@protonmail.com>
2019-04-24 21:08:23 -0500

    fixed jquery for modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,31 +15,32 @@ module.exports = function (app) {
             friendDiff: 500
         };
         var userData = req.body;
-        var userName = userData.name;
+        // var userName = userData.name;
         var userScores = userData.scores;
         var scoreInt = userScores.map(function(int) {
             return parseInt(int, 10);
         });
 
         userData = {
-            "name": req.body.name,
-            "photo": req.body.photo,
-            "scores": scoreInt
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: scoreInt
         };
 
         for (var i = 0; i < friendData.length; i++) {
-
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-                totalDifference += Math.abs(scoreInt[j] - parseInt(friends[i].scores[j]));
+            // totalDifference = 0;
+            for (var j = 0; j < friendData[i].scores[j]; j++) {
+                totalDifference += Math.abs(scoreInt[j] - parseInt(friendData[i].scores[j]));
             }
             if (totalDifference <= mostComp.friendDiff) {
-                mostComp.name = friends[i].name;
-                mostComp.photo = friends[i].photo;
+                mostComp.name = friendData[i].name;
+                mostComp.photo = friendData[i].photo;
                 mostComp.friendDiff = totalDifference;
             }
             
         };
         console.log ("total difference is " + totalDifference);
+        console.log ("friend difference is " + mostComp.friendDiff);
 
 
         friendData.push(userData);
