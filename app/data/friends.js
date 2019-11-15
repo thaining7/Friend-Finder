45b5de3d0baf499c9f27dd4d2b5ff98c84cb5cef
thaining7 <thaining@protonmail.com>
2019-11-15 12:34:39 -0600

    updated friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,10 @@
 var friendArray = [
     {
+        "name": "Barbara Seville",
+        "photo": "",
+        "scores": [5, 4, 1, 5, 2, 5, 3, 2, 5, 2]
+    },
+    {
         "name": "Eureka Garlic",
         "photo": "",
         "scores": [5, 1, 2, 2, 4, 1, 3, 5, 3, 5]
